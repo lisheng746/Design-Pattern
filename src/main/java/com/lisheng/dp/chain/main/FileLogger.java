59f58f055bfe24a8452434d6d8bc1bcf7a2a6d13
李晟 <lisheng@walkclass.com>
2018-02-05 10:20:32 +0800

    add responsibility chain demo

---
+++ src/main/java/com/lisheng/dp/chain/main/FileLogger.java
@@ -0,0 +1,13 @@
+package com.lisheng.dp.chain.main;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
